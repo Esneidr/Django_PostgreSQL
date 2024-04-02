de434834a2b23b7a249383819e4612e25205e363
Esneidr <eacordobam@gmail.com>
2024-04-01 23:36:01 -0500

    Finalizando el proyecto

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Taks
 
 # Create your views here.
 def list_tasks(request):
-    return render(request, 'list_tasks.html')
+    tasks = Taks.objects.all()
+    return render(request, 'list_tasks.html', {"tasks": tasks })
+
+def create_task(request):
+    task = Taks(title=request.POST['title'], description=request.POST['description'])
+    task.save()
+    return redirect('/tasks/')
+
+def delete_task(request, task_id):
+    task = Taks.objects.get(id=task_id)
+    task.delete()
+    return redirect('/tasks/')
+
